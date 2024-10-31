cb9b28e62dfbb57c089d22dc8fcd4eb1fbf32d7d
Cristian Marsico <cristianmarsico84@gmail.com>
2024-10-31 18:06:30 -0300

    conexion

--- database/db_estudio_juridico.js
+++ database/db_estudio_juridico.js
@@ -7,11 +7,26 @@ const conexion = new Pool({
     // password: '31317933',
     // port: 5432,
 
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_DATABASE,
-    password: process.env.DB_PASS,
-    port: 5432,
+    // user: process.env.DB_USER,
+    // host: process.env.DB_HOST,
+    // database: process.env.DB_DATABASE,
+    // password: process.env.DB_PASS,
+    // port: 5432,
+    connectionString: process.env.DB_DATABASE,
+    ssl:true
 });
+
+// Función para probar la conexión
+async function testConnection() {
+    try {
+        const res = await conexion.query('SELECT 1');
+        console.log('Conexión exitosa a la base de datos:', res.rows);
+    } catch (error) {
+        console.error('Error al conectar con la base de datos:', error);
+    }
+}
+
+// Llama a la función de prueba
+testConnection();
 
 module.exports = conexion;
