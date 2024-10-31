ec9523af56d509e4c9fad226815600da811ac238
Cristian Marsico <cristianmarsico84@gmail.com>
2024-10-31 17:19:35 -0300

    variables de entorno

--- database/db_estudio_juridico.js
+++ database/db_estudio_juridico.js
@@ -1,11 +1,17 @@
 const { Pool } = require('pg');
 
 const conexion = new Pool({
-    user: 'postgres',          // Usuario de la base de datos
-    host: 'localhost',          // Host (generalmente localhost)
-    database: 'db_estudio_juridico',  // Nombre de la base de datos
-    password: '31317933',  // Contraseña del usuario
-    port: 5432,                 // Puerto de PostgreSQL (por defecto 5432)
+    // user: 'postgres',
+    // host: 'localhost',
+    // database: 'db_estudio_juridico',
+    // password: '31317933',
+    // port: 5432,
+
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_DATABASE,
+    password: process.env.DB_PASS,
+    port: 5432,
 });
 
 module.exports = conexion;
--- index.js
+++ index.js
@@ -2,7 +2,14 @@
 
 const express = require('express');
 const cookie = require('cookie-parser');
+const dontev = require('dotenv');
 const app = express();
+
+const path = require('path');
+//SETEAMOS VARIABLES DE ENTORNO
+dontev.config({
+    path: '.env'
+})
 
 //HACEMOS USO DE LAS COOKIES
 app.use(cookie())
