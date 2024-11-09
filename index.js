797f7cf9d2dcf4ac245c71330040de770ea16c52
Cristian Marsico <cristianmarsico84@gmail.com>
2024-11-09 19:27:46 -0300

    cors dotenv nuevos

--- index.js
+++ index.js
@@ -7,45 +7,45 @@ const cors = require('cors');
 
 const app = express();
 
-//HACEMOS USO DE LAS COOKIES
-app.use(cookie())
-const allowAnyLocalhost = process.env.ALLOW_ANY_LOCALHOST === 'true';
-
+// Cargamos las variables de entorno
 dontev.config({
     path: '.env'
-})
+});
+
+// Usamos las cookies
+app.use(cookie());
+
+// Configuración de CORS
+const allowAnyLocalhost = process.env.ALLOW_ANY_LOCALHOST === 'true';
+const allowedOrigins = [
+    process.env.ORIGIN1,  // Producción
+    allowAnyLocalhost ? /^http:\/\/localhost(:\d+)?$/ : null  // Desarrollo
+].filter(Boolean);  // Filtramos los valores nulos
 
 app.use(cors({
-    origin: allowAnyLocalhost ? /^http:\/\/localhost(:\d+)?$/ : function (origin, callback) {
-        if (!origin) {
-            return callback(null, 'http://localhost');
+    origin: function (origin, callback) {
+        // Permitir solicitudes sin origen (como desde herramientas de prueba) o desde orígenes permitidos
+        if (!origin || allowedOrigins.some(o => o instanceof RegExp ? o.test(origin) : o === origin)) {
+            return callback(null, true);
         }
-
-        // Agrega lógica adicional aquí si es necesario
-
-        return callback("Error de CORS origin: " + origin + " No autorizado");
+        callback(new Error(`Error de CORS. Origen no autorizado: ${origin}`));
     },
     credentials: true,
-    exposedHeaders: allowAnyLocalhost ? null : ['Content-Length', 'Authorization'], // Ajusta según tus necesidades
+    exposedHeaders: ['Content-Length', 'Authorization']
 }));
-
 app.options('*', cors());
 
+// Middleware para JSON y datos de formularios
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-//VAMOS A TRABAJAR CON JSON
-app.use(express.json())
-
-    //PARA PROCESAR DATOS ENVIADOS DESDE FORMS
-    .use(express.urlencoded({
-        extended: true
-    }))
-
+// Rutas
 app.use('/api', require('./routes/FormRouter.js'));
 app.use('/api', require('./routes/ClienteRouter.js'));
 app.use('/api', require('./routes/AnalisisRouter.js'));
 app.use('/api', require('./routes/MovimientoRouter.js'));
 
-
+// Selección del puerto según el modo
 const PORT = process.env.MODO === 'developer' ? process.env.PORT_DEV : process.env.PORT_PROD;
 app.listen(PORT, () => {
     console.log(`Servidor escuchando en el puerto ${PORT}`);
