5747e1284d0b79eef5b29d777ecab4a7f5103214
SebasPez <dr.sebastianjauregui@gmail.com>
2025-04-05 15:11:40 -0300

    cambiamos el ssl

--- database/Conexion.js
+++ database/Conexion.js
@@ -28,9 +28,7 @@ const conexion = new Pool({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    ssl: process.env.MODO === 'produccion'
-        ? false
-        : false
+    ssl: false
 });
 
 /**
