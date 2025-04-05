66c71a750e58496a5634cfe19c028bac33733a22
SebasPez <dr.sebastianjauregui@gmail.com>
2025-04-05 16:42:42 -0300

    Update Conexion.js

--- database/Conexion.js
+++ database/Conexion.js
@@ -41,7 +41,7 @@ const conexion = new Pool({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    ssl: false
+    ssl: { rejectUnauthorized: false }
 });
 
 console.log("🔍 Conectando a la base con config:", {
